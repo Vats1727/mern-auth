e81da662e0f0f1e748bfd5e7e362b6f9e77e1838
Vats1727 <vatsalparmar1742002@gmail.com>
2025-08-31 16:20:16 +0530

    upadte in register api due to some iusse now working perfectly

---
+++ backend/routes/auth.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";  
+const router = express.Router();
+
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+  
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export default router;
